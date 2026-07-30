97b9116567cf8d4a6d8bdfe27f545970fd94e244
Narmin Aliyeva <narminaliyeva484@gmail.com>
2026-07-30 16:13:39 +0400

    Add multiple pages and Flask routes

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app=Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/trips")
+def trips():
+    return render_template("trips.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__== "__main__":
     app.run(debug=True)
 
