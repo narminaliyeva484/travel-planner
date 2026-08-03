401e9fc7c387bf202cbe40b7283275e9ccefd6fa
Narmin Aliyeva <narminaliyeva484@gmail.com>
2026-08-03 19:13:13 +0400

    Implement full CRUD functionality for trips

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 
@@ -8,7 +8,6 @@ def get_db_connection():
     conn= sqlite3.connect("travel_planner.db")
     conn.row_factory = sqlite3.Row
     return conn
-
 
 
 @app.route("/")
@@ -22,21 +21,8 @@ def about():
 @app.route("/trips", methods=["GET", "POST"])
 def trips_page():
 
-    if request.method == "POST":
-        destination= request.form["destination"]
-        date= request.form["date"]
-
-        db= get_db_connection()
-
-        db.execute(
-            "INSERT INTO trips (destination, date) VALUES (?, ?)", (destination, date)
-        )
-
-        db.commit()
-        db.close()
-
     db= get_db_connection()
-    
+
     trips = db.execute(
         "SELECT * FROM trips"
     ).fetchall()
@@ -45,9 +31,72 @@ def trips_page():
 
     return render_template("trips.html", trips=trips)
 
+
+@app.route("/add-trip", methods=["GET","POST"])
+def add_trip():
+
+    if request.method == "POST":
+
+        destination = request.form["destination"]
+        date = request.form["date"]
+
+        db = get_db_connection()
+
+        db.execute(
+            "INSERT INTO trips (destination, date) VALUES (?, ?)",
+            (destination, date)
+        )
+
+        db.commit()
+        db.close()
+
+        return redirect("/trips")
+
+    return render_template("add_trip.html")
+
+@app.route("/delete/<int:trip_id>", methods=["POST"])
+def delete_trip(trip_id):
+    db = get_db_connection()
+
+    db.execute(
+        "DELETE FROM trips WHERE id = ?",
+        (trip_id,)
+    )
+
+    db.commit()
+    db.close()
+
+    return redirect(url_for("trips_page"))
+
+
+@app.route("/edit/<int:trip_id>" , methods=["GET", "POST"])
+def edit_trip(trip_id):
+    db= get_db_connection()
+
+    if request.method=="POST":
+
+        destination= request.form["destination"]
+        date= request.form["date"]
+
+        db.execute("UPDATE trips SET destination = ? , date = ? WHERE id = ?", (destination, date, trip_id))
+
+        db.commit()
+        db.close()
+
+        return redirect("/trips")
+
+    trip = db.execute("SELECT * FROM trips WHERE id = ?", (trip_id,)).fetchone()
+
+    db.close()
+
+    return render_template("edit_trip.html", trip=trip)
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
 
 
 if __name__== "__main__":
